fba5ca9f29cc3200f0e0a61b5e7b5032c3e007f8
nikibobi <boby_dsm@abv.bg>
2013-03-24 18:24:30 +0200

    Optimized PostRequest method

--- PastebinAPI/Utills.cs
+++ PastebinAPI/Utills.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
 using System.Text;
 
@@ -11,33 +8,20 @@ namespace PastebinAPI
     {
         public static string PostRequest(string url, string[] parameters)
         {
-            string postString = string.Join("&", parameters);
             WebRequest request = WebRequest.Create(url);
             request.Method = "POST";
-            byte[] byteArray = Encoding.UTF8.GetBytes(postString);
             request.ContentType = "application/x-www-form-urlencoded";
+            string postString = string.Join("&", parameters);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postString);
             request.ContentLength = byteArray.Length;
-            using (var dataStream = request.GetRequestStream())
+            using (Stream dataStream = request.GetRequestStream())
             {
                 dataStream.Write(byteArray, 0, byteArray.Length);
             }
-            using (var response = request.GetResponse())
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
             {
-                if (((HttpWebResponse)response).StatusDescription != "OK")
-                {
-                    Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-                }
-                using (var dataStream = response.GetResponseStream())
-                {
-                    if (dataStream != null)
-                    {
-                        using (var reader = new StreamReader(dataStream))
-                        {
-                            return reader.ReadToEnd();
-                        }
-                    }
-                    throw new NullReferenceException("dataStream from responce is null");
-                }
+                return reader.ReadToEnd();
             }
         }
     }
