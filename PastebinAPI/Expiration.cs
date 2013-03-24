c8c28ec2513347889401b5cd8a65ff26b130d2eb
nikibobi <boby_dsm@abv.bg>
2013-03-24 16:44:57 +0200

    Created PasteFormat class

--- PastebinAPI/Expiration.cs
+++ PastebinAPI/Expiration.cs
@@ -7,13 +7,12 @@ namespace PastebinAPI
 {
     public class Expiration
     {
-        public const string DFAULT = "N";
-
         public static readonly Expiration Never = new Expiration("N");
         public static readonly Expiration TenMinutes = new Expiration("10M");
         public static readonly Expiration OneHour = new Expiration("1H");
         public static readonly Expiration OneDay = new Expiration("1D");
         public static readonly Expiration OneMonth = new Expiration("1M");
+        public static readonly Expiration Default = Never;
 
         private readonly string value;
         private Expiration(string value)
