af317ff62f3d33732780fad39d213cb9bbd4875d
Borislav Kosharov <boby_dsm@abv.bg>
2013-04-17 16:45:39 +0300

    Added one week and two weeks expirations

--- PastebinAPI/Expiration.cs
+++ PastebinAPI/Expiration.cs
@@ -6,6 +6,8 @@
         public static readonly Expiration TenMinutes = new Expiration("10M");
         public static readonly Expiration OneHour = new Expiration("1H");
         public static readonly Expiration OneDay = new Expiration("1D");
+        public static readonly Expiration OneWeek = new Expiration("1W");
+        public static readonly Expiration TwoWeeks = new Expiration("2W");
         public static readonly Expiration OneMonth = new Expiration("1M");
         public static readonly Expiration Default = Never;
 
