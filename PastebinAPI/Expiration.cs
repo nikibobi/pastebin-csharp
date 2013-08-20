13b4d118c5decbf2a44c070b6e0ec845285a85cf
Borislav Kosharov <boby_dsm@abv.bg>
2013-08-20 19:24:39 +0300

    Optimized Expiration

--- PastebinAPI/Expiration.cs
+++ PastebinAPI/Expiration.cs
@@ -6,38 +6,38 @@ namespace PastebinAPI
 {
     public class Expiration
     {
+        public static readonly Expiration Never;
         public static readonly Expiration TenMinutes;
         public static readonly Expiration OneHour;
         public static readonly Expiration OneDay;
         public static readonly Expiration OneWeek;
         public static readonly Expiration TwoWeeks;
         public static readonly Expiration OneMonth;
-        public static readonly Expiration Never;
         public static readonly Expiration Default;
         public static readonly IEnumerable<Expiration> All;
 
         static Expiration()
         {
+            Never = new Expiration("N", TimeSpan.Zero);
             TenMinutes = new Expiration("10M", TimeSpan.FromMinutes(10));
             OneHour = new Expiration("1H", TimeSpan.FromHours(1));
             OneDay = new Expiration("1D", TimeSpan.FromDays(1));
             OneWeek = new Expiration("1W", TimeSpan.FromDays(7));
             TwoWeeks = new Expiration("2W", TimeSpan.FromDays(14));
             OneMonth = new Expiration("1M", TimeSpan.FromDays(30));
-            Never = new Expiration("N", TimeSpan.MaxValue);
             Default = Never;
             All = getExpirations();
         }
 
         private static IEnumerable<Expiration> getExpirations()
         {
+            yield return Never;
             yield return TenMinutes;
             yield return OneHour;
             yield return OneDay;
             yield return OneWeek;
             yield return TwoWeeks;
             yield return OneMonth;
-            yield return Never;
         }
 
         public static Expiration Parse(string s)
@@ -68,11 +68,8 @@ namespace PastebinAPI
 
         public static Expiration FromTimeSpan(TimeSpan timeSpan)
         {
-            //return All.First(e => timeSpan <= e.time); //maybe use this?
-            foreach (Expiration expiration in All)
-                if (timeSpan <= expiration.time)
-                    return expiration;
-            throw new InvalidOperationException(string.Format("Unreachable code reached! timeSpan = {0}", timeSpan));
+            //if timespan <= 0 or > OneMonth then return never
+            return All.FirstOrDefault(e => timeSpan <= e.time) ?? Never;
         }
 
         private readonly string value;
